11786ee965a28b7f6b3bc3d869a3ae59f99ab167
FikiRiloPambudi <64366136+FikiRiloPambudi@users.noreply.github.com>
2022-11-20 17:41:43 +0700

    add extention chart

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -13,13 +13,15 @@
     <!-- Plugin css for this page -->
     <link rel="stylesheet" href="{{asset('admin/vendors/daterangepicker/daterangepicker.css')}}">
     <link rel="stylesheet" href="{{asset('admin/vendors/chartist/chartist.min.css')}}">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
     <link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/styleku.css')}}">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="{{asset('admin/images/favicon.png')}}" />
-    <link rel="stylesheet" href="{{url('admin/css/styleku.css')}}">
   </head>
   <body>
