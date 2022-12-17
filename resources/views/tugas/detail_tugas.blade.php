1eec4c913fb9c69f955d5d03386d3554c5a833aa
20043 <fahmi.muhazir.12@gmail.com>
2022-12-17 11:50:23 +0700

    layouting view tugas

--- resources/views/tugas/detail_tugas.blade.php
+++ resources/views/tugas/detail_tugas.blade.php
@@ -1,18 +1,17 @@
 @extends('admin.index')
 @section('content')
-<section class="section profile">
-    <div class="row">
-        <div class="col-xl-12">
-            <div class="card">
-                <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+<div class="main-panel">
+    <div class="content-wrapper">
+            {{-- <div class="card d-flex" style="background-color: white; height:100%;"> --}}
+                <div class="card-body tugas-card pt-4 d-flex flex-column align-items-center" style="">
                 <!-- // ============================= Fergi : Detail Tugas ========================= -->
-                    <h2>{{ $row->keterangan }}</h2>
+                    {{-- <h2>{{ $row->keterangan }}</h2> --}}
                     <h3>{{ $row->perihal }}</h3>
                     @php
                         if(!empty($row->upload)){
                     @endphp
                         
-                        <iframe src="{{asset('admin/images/tugas') }}/{{ $row->upload }}" width="600" height="400"></iframe>
+                        <iframe src="{{asset('admin/images/tugas') }}/{{ $row->upload }}" class="image-task" width="80%" height="320"></iframe>
                         
                     @php
                         }else{
@@ -22,17 +21,17 @@
                         }
                     @endphp
                     
-                    <!-- <div class=" alert alert-primary" role="alert">
-                        <br />Tempat Lahir : {{ $row->tempat_lahir }}
-                        <br />Tanggal Lahir: {{ $row->tgl_lahir }}     
-                        <br />Jenis Kelamin: {{ $row->gender }}
-                        <br />Alamat       : {{ $row->alamat }}
-                        <br />Email        : {{ $row->email }}
-            
-                    </div> -->
+                    <!-- <div class=" alert alert-primary" role="alert"></div> -->
+                        <br /> Tanggal Pengumpulan : {{ $row->tanggal }}
+                        <br />Jam: {{ $row->jam }}     
+                        <br />Keterangan Tugas: {{ $row->keterangan }}
+                        {{-- @if(!empty($row->upload))
+                            <br />Foto: <img src="{{ asset('admin/images/tugas') }}/{{ $row->upload }}" width="100" height="100">
+                        @endif --}}
+                    
                 </div>
-            </div>
+            {{-- </div> --}}
         </div>
-    </div>
-</section>
+{{-- </section> --}}
+</div>
 @endsection
